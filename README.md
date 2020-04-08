155447533f7d3346db22ecd3f98f4a589919283d
mackraja <montykhanna007@hotmail.com>
2020-04-08 18:06:00 +0530

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# getting-react
+
+* https://getting-react.herokuapp.com/
+
+### Local Environment Setup
+* Use Node Version: ^10.x.x
+* Use NPM Version: ^6.4.1
+
+##### Clone Project: 
+
+* Using SSH: git clone git@bitbucket.org:MackRaja/getting-react.git
+* Using HTTPS: git clone https://MackRaja@bitbucket.org/MackRaja/getting-react.git
+
+##### Install Node Packages:
+
+* Using npm: npm install 
+* Using yarn: yarn install
+
+##### Run Client App: 
+
+* Using npm: npm start
+* Using yarn: yarn run start
---
+++ src/redux/ApiClient.js
@@ -0,0 +1,83 @@
+/**
+ * @author {[Monty Khanna]}
+ */
+import axios from 'axios';
+import { omitBy, isUndefined, isNull } from 'lodash';
+import store from 'store2';
+import {} from 'dotenv/config';
+
+const methods = ['get', 'post', 'put', 'patch', 'del'];
+
+const apiPath = process.env.REACT_APP_BASE_URL;
+
+console.log('apiPath ----------------- ', apiPath);
+
+function formatUrl(path) {
+  if (!path) {
+    return path;
+  }
+
+  if (/^(\/\/|http|https)/.test(path)) {
+    return path;
+  }
+  const adjustedPath = path[0] !== '/' ? `/${path}` : path;
+  return `${apiPath}${adjustedPath}`;
+}
+
+export default class ApiClient {
+  constructor(req) {
+    methods.forEach((method) => {
+      this[method] = this._req(method);
+    });
+  }
+
+  _req = method => (path, { params, data, files } = {}) => new Promise((resolve, reject) => {
+      const apiObj = {
+          method,
+          url: formatUrl(path),
+      };
+
+      if (params) {
+        const emptySting = val => val === '';
+        const cleanFromUndefined = omitBy(params, isUndefined);
+        const cleanFromNull = omitBy(cleanFromUndefined, isNull);
+        const localParam = omitBy(cleanFromNull, emptySting);
+        apiObj.params = localParam || {};
+      }
+
+      if (data) {
+        apiObj.data = data;
+      }
+
+      const http = axios.create ({
+          baseURL: apiPath,
+          timeout: 10000,
+          withCredentials: true,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+      });
+
+      http.interceptors.request.use (
+          (config) => {
+              const token = store('authToken');
+              const refreshToken = store('refreshToken');
+
+              if (token) config.headers.Authorization = `Bearer ${token}`;
+              if (refreshToken) config.headers.RefreshToken = refreshToken;
+              return config;
+          },
+          (error) => reject (error)
+      );
+
+      http(apiObj)
+          .then((response) => {
+              resolve(response.data);
+          })
+          .catch((error) => {
+              console.log('Error: To call API', error);
+              reject(error);
+          });
+  });
+}
